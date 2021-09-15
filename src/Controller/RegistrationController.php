936403ee3e7952b21e8c6f7421f3a9540c9b1baf
unknown <nemexxix@gmail.com>
2021-09-15 12:08:53 +0200

    redirection sur l'index après le register

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -53,7 +53,7 @@ class RegistrationController extends AbstractController
             ); */
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('app_main');
+            return $this->redirectToRoute('index');
         }
 
         return $this->render('registration/register.html.twig', [
